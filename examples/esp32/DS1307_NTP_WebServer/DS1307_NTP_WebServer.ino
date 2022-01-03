599810954ef51378276e6313a6bb12955489767e
3tawi <96850409+3tawi@users.noreply.github.com>
2022-01-03 17:15:15 +0100

    Update DS1307_NTP_WebServer.ino

--- examples/esp32/DS1307_NTP_WebServer/DS1307_NTP_WebServer.ino
+++ examples/esp32/DS1307_NTP_WebServer/DS1307_NTP_WebServer.ino
@@ -42,6 +42,7 @@ void getWifi() {
     Serial.println(WiFi.localIP().toString().c_str());
     WiFi.softAPdisconnect(true);
     } else { 
+    WiFi.softAPConfig(ip, gatewayap, subnet);
     WiFi.softAP(Apssid, Appassword);
     Serial.println("CONNECTED To: NeoPixel");
     Serial.println("Password: '3tawi-GP'");
